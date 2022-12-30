9f8ad35894cc42c3003567202f91071b73b31918
abdullah1011997 <121689071+abdullah1011997@users.noreply.github.com>
2022-12-30 21:43:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, sum;
+            int n1, n2;
+            Console.WriteLine("The perfec number");
+
+            Console.Write("Enter The First Number: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter The Second Nuumber: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.Write("The perfect numbers is:\n");
+            for (n = n1; n <= n2; n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+
+                    i++;
+
+
+                }
+                if (sum == n)
+                    Console.WriteLine("{0}", n);
+
+
+            }
+
+        }
+    }
+}
